eb4a3ae18e4f8a4fcc3aab9b6f43fdb1b5990ac6
Abdelsatter11 <mohamedaladawy159753@gmail.com>
2023-11-27 23:31:52 +0200

    test33

--- src/User.java
+++ src/User.java
@@ -2,6 +2,5 @@ public abstract class User {
     public int id;
     public String password;
     public String userName;
-    public double salary;
     public abstract void EditPersonalInformation();
 }
