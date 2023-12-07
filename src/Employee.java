2e68206a285de77745000bdc037c5591e8951ca8
keromagdy80 <keromagdy807@gmail.com>
2023-12-07 20:47:10 +0200

    Create & Delete Account

--- src/Employee.java
+++ src/Employee.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
+import java.util.List ;
 
 public class Employee extends User{
-    public void CreateClientAccount(){
+    public void CreateClientAccount()
+    {
+        Lists.clients.add(new Client());
+    }
+    public void EditClientAccount(Client client)
+    {
 
     }
-    public void EditClientAccount(Client client){}
-    public void DeleteClientAccount(Client client){
+    public void DeleteClientAccount(int ID)
+    {
+       Lists.clients.remove(ID-1) ;
+    }
+    public Client SearchForClientAccount(int ID)
+    {
+      return   Lists.clients.get(ID-1);
 
     }
-    public void SearchForClientAccount(String clientName){
 
     }
-}
+
